77dfb0e3f96017a6afa99ba189332f71fb8e4432
mrzogy <magbolmrzogy@gmail.com>
2023-05-21 17:49:31 +0300

    Initial Commit

---
+++ Model/ModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelApp.swift
+//  Model
+//
+//  Created by 3bood on 01/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
